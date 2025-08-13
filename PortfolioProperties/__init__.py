46d2c06204c72a13d7e825962e4ebcbdff89a481
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-08-13 13:55:58 -0500

    purge

--- PortfolioProperties/__init__.py
+++
@@ -1,5 +0,0 @@
-import azure.functions as func
-from .common import handle
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return handle('property_occupancy_v', req)
--- PortfolioUnits/__init__.py
+++
@@ -1,5 +0,0 @@
-import azure.functions as func
-from .common import handle
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return handle('units_v', req)
